5c58abc80f7e85e518504f7eef29fc38eabd1345
Paul Drummond <paul.drummond@iode.co.uk>
2015-07-17 22:16:14 +0100

    Added simple status filter

--- packages/cardlist-view/client/cardlist-view.js
+++ packages/cardlist-view/client/cardlist-view.js
@@ -1,8 +1,34 @@
 Template.cardListView.helpers({
 	cards: function() {
-		return Boom.CardCollections.TaskCard.find({boardId: Session.get('currentBoardId')});
-	}
+		var criteria = {
+			boardId: Session.get('currentBoardId'),
+		}
+		var statusFilter = Session.get('cardListView.statusFilter');
+		if(statusFilter) {
+			criteria.status = statusFilter.value;
+		}
+		return Boom.CardCollections.TaskCard.find(criteria);
+	},	
+
+	selectedStatus: function() {
+		var label = "All";
+		var statusFilter = Session.get('cardListView.statusFilter');
+		if(statusFilter) {
+			label = statusFilter.label;
+		}
+		return label;
+	},
+
+	statusFields: function() {
+		return Boom.CardTemplates["TaskCard"].fields["status"].autoform.options;
+	},	
 });
+
+Template.cardListView.events({
+	'click #clear-status-filter': function() {
+		Session.set('cardListView.statusFilter', null);
+	}
+})
 
 Template.cardItem.events({
 	'click #show-more-button': function() {		
@@ -13,3 +39,9 @@ Template.cardItem.events({
 Template.cardListView.onRendered(function() {
 	this.$('.ui.dropdown').dropdown();
 });
+
+Template.statusItem.events({
+	'click': function() {
+		Session.set('cardListView.statusFilter', this);
+	}
+})
