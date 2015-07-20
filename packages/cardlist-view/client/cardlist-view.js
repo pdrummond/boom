0615032343f8f296ea375a8f962ee91ed2200270
Paul Drummond <paul.drummond@iode.co.uk>
2015-07-20 21:50:23 +0100

    Added support for archived

--- packages/cardlist-view/client/cardlist-view.js
+++ packages/cardlist-view/client/cardlist-view.js
@@ -1,50 +1,53 @@
+
 Template.cardListView.helpers({
-	cards: function() {
-		var criteria = {
-			boardId: Session.get('currentBoardId'),
-			archived: false			
-		}
-		var statusFilter = Session.get('cardListView.statusFilter');
-		if(statusFilter) {
-			criteria.status = statusFilter.value;
-		}
-		var archivedFilter = Session.get('cardListView.archivedFilter');
-		if(archivedFilter) {
-			criteria.archived = archivedFilter.value;
-		}		
-		return Boom.CardCollections.TaskCard.find(criteria, {sort: {updatedAt: -1}});
+	cards: function() {				
+		return Boom.CardCollections.TaskCard.find(SearchCriteria.getCriteria(), {sort: {updatedAt: -1}});
 	},	
 
-	numCards: function() {
-		//TODO: Don't duplicate criteria like this - come on!
-		var criteria = {
-			boardId: Session.get('currentBoardId'),
-		}
-		var statusFilter = Session.get('cardListView.statusFilter');
-		if(statusFilter) {
-			criteria.status = statusFilter.value;
-		}		
-		return Boom.CardCollections.TaskCard.find(criteria).fetch().length;
+	numCards: function() {		
+		return Boom.CardCollections.TaskCard.find(SearchCriteria.getCriteria()).fetch().length;
+	},
+
+	numArchivedCards: function() {
+		return Boom.CardCollections.TaskCard.find(SearchCriteria.getArchivedCriteria()).fetch().length;
 	},
 
 	selectedStatus: function() {
-		var label = "All";
+		var label = "all";
 		var statusFilter = Session.get('cardListView.statusFilter');
 		if(statusFilter) {
 			label = statusFilter.label;
+		}
+		return label.toLowerCase();
+	},
+
+	selectedVisibility: function() {
+		var label = "active";
+		var archived = Session.get('cardListView.archivedFilter');
+		if(archived) {
+			label = "archived";
 		}
 		return label;
 	},
 
 	statusFields: function() {
-		return Boom.CardTemplates["TaskCard"].fields["status"].autoform.options;
+		return Boom.CardTemplates["TaskCard"].fields["status"].values;
 	},	
 });
 
 Template.cardListView.events({
 	'click #clear-status-filter': function() {
 		Session.set('cardListView.statusFilter', null);
-	}
+	},
+	
+	'click #set-active-filter': function() {
+		Session.set('cardListView.archivedFilter', false);
+	},
+
+	'click #set-archived-filter': function() {
+		Session.set('cardListView.archivedFilter', true);
+	},
+
 })
 
 Template.cardItem.helpers({
@@ -54,6 +57,10 @@ Template.cardItem.helpers({
 		} else {
 			return "Last updated ";
 		}
+	},
+
+	archivedClass: function() {		
+		return this.archived ? "archived" : "";
 	}
 });
 
@@ -71,4 +78,35 @@ Template.statusItem.events({
 	'click': function() {
 		Session.set('cardListView.statusFilter', this);
 	}
-})
+});
+
+SearchCriteria = {
+	getCriteria: function() {
+		var criteria = {
+			boardId: Session.get('currentBoardId'),
+			archived: false			
+		}
+		var statusFilter = Session.get('cardListView.statusFilter');
+		if(statusFilter) {
+			criteria.status = statusFilter.value;
+		}
+		var archivedFilter = Session.get('cardListView.archivedFilter');
+		if(archivedFilter) {
+			criteria.archived = archivedFilter;
+		}
+		return criteria;
+	},
+
+	getArchivedCriteria: function() {
+		var criteria = {
+			boardId: Session.get('currentBoardId'),
+			archived: false			
+		}
+		var statusFilter = Session.get('cardListView.statusFilter');
+		if(statusFilter) {
+			criteria.status = statusFilter.value;
+		}		
+		criteria.archived = true;
+		return criteria;
+	}
+};
